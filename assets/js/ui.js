2e75182899c0ec571409c2efacf95c8f46cd09b5
Mae Cana <mae.cana@outlook.com>
2021-01-24 22:14:15 +0800

    Added initial modal for restart/game over UI

---
+++ assets/js/ui.js
@@ -0,0 +1,8 @@
+addEventListener('DOMContentLoaded', (e) => {
+    // $('#gameOverModal').on('show.bs.modal', function (event) {
+    //     var button = $(event.relatedTarget);
+    //     var modal = $(this)
+    // });
+
+    $("#restartModal").modal('show');
+});
